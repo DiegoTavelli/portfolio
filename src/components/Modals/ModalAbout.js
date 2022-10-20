a1a531f93b03f5dd70d4f6e53be816c9f51c6596
DiegoTavelli <diegotavelli@gmail.com>
2022-10-20 16:14:06 -0300

    aboutModal CvLink

--- src/components/Modals/ModalAbout.js
+++ src/components/Modals/ModalAbout.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import aboutModal from '../../assets/figma/aboutModal4.png'
 import './modalAbout.scss'
+import CvButton from '../../assets/figma/cvButton.png'
+import Arrow from '../../assets/figma/arrow.png'
 
-import './modalProject.scss'
+
 
 const ModalAbout = ({ about }) => {
 
-
+  const linKCv = () => {
+    window.open('https://drive.google.com/file/d/1OCPm5BnMWHRuXxAqNIyGDx7sLTlXRd4W/view?usp=sharing');
+  }
 
   return about ?
     <div className='modalAboutContainer'>
       <img src={aboutModal} alt='' className={about ? 'aboutModalIn' : 'aboutModalOut'} />
+      <div className='buttonCvHover'>
+        <img src={Arrow} className='ArrowAbout' alt='' />
+        <img onClick={linKCv} src={CvButton} className='CvButton' alt='' />
+      </div>
     </div>
     : null
 
